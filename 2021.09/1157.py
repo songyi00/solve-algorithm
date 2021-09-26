552b45808402bebc12760d893055d9aa20fab50f
sonyi00 <ksl2950@naver.com>
2021-09-26 14:08:33 +0900

    upload

---
+++ 2021.09/1157.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+temp = input().upper()
+dic = defaultdict(int)
+
+for t in temp:
+    dic[t]+=1
+
+val = 0
+result = ''
+cnt = 0
+for k,v in dic.items():
+    if val < v :
+        result = k
+        val = v 
+    elif val == v:
+        cnt+=1
+if not cnt:
+    print(result)
+else:
+    print('?')
+
+#print(dic)
