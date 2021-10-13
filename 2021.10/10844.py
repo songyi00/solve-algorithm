34a42b74ccc35e5f9adec7937ce53b44f1326161
sonyi00 <ksl2950@naver.com>
2021-10-13 19:30:52 +0900

    upload

---
+++ 2021.10/10844.py
@@ -0,0 +1,17 @@
+n = int(input())
+dp = [[0]*(10) for _ in range(101)]
+
+for i in range(1,10):
+    dp[1][i] = 1
+
+#dp[자리수][끝 숫자]
+for i in range(2,n+1):
+    for j in range(10):
+        if j == 0:
+            dp[i][j] = dp[i-1][j+1]
+        elif 1<=j<=8:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+        else:
+            dp[i][j] = dp[i-1][j-1]
+
+print(sum(dp[n])%(10**9))
